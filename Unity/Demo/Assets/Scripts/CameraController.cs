3f37848de5871aed73a6cd3c29cf268ac7a3e721
dschwenk <mail@danielschwenk.de>
2015-10-25 10:12:46 +0100

    imported assets from 'roll a ball' tutorial

---
+++ Unity/Demo/Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        // Abstand Kamera - Spieler
+        offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame (LateUpdate -> it is run after "all other things are done" / after we know the player object has moved)
+	void LateUpdate () {
+        // 
+        transform.position = player.transform.position + offset;
+	}
+}
---
+++ Unity/Demo/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int count;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+	// Update is called once per frame
+    // is run before rendering a frame
+	void Update ()
+    {
+	
+	}
+
+    // is ran before performing any physics calculation
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+
+    void OnTriggerEnter(Collider other)// wird ausgefuehrt, wenn der Spieler mit einem anderen TriggerCollider kollidiert
+    {
+        if(other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 13)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
