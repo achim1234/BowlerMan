95ec9126458959d1cb6a03c8920c2240e5bee364
dschwenk <mail@danielschwenk.de>
2015-11-02 18:12:00 +0100

    updated player controller script - added health variable, added comments

--- Unity/Demo/Assets/Scripts/PlayerController.cs
+++ Unity/Demo/Assets/Scripts/PlayerController.cs
@@ -1,22 +1,29 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 
 
 public class PlayerController : MonoBehaviour {
 
-    public float speed;
-    public bool noPlayerStop; // if player is too slow, there is a speeding up
+    // player settings
+	private Rigidbody rb;
+	
+	public int health = 100;
 
+    private Vector3 lastPosition; // last position of player - needed to calculate speed of player
+    private float plyayerSpeed = 0; // speed of player - at start 0	
+	
+	public bool noPlayerStop; // if player is too slow, there is a speeding up
+	
+	public float speedMultiplier; // multiplier to adjust speed of player
+    
+    private int count; // counter - how much pick ups were picked
+
+	
     // UI elements
     public Text countText;
     public Text winText;
 
-    private Rigidbody rb;
-    private int count;
-
-    private Vector3 lastPosition;
-    private float plyayerSpeed = 0;
 
 
     void Start ()
@@ -31,7 +38,7 @@ public class PlayerController : MonoBehaviour {
     // is run before rendering a frame
 	void Update ()
     {
-	
+
 	}
 
     // is ran before performing any physics calculation
@@ -55,10 +62,9 @@ public class PlayerController : MonoBehaviour {
             }
         }
 
-
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-        rb.AddForce(movement * speed);
+        rb.AddForce(movement * speedMultiplier);
     }
 
 
