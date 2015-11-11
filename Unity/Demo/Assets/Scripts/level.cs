9c95ed44038a0cb9d0b2fa89f4d021d65dca1c13
Werner <Werner91@googlemail.com>
2015-11-11 12:57:20 +0100

    Menu Options Button eingefügt, Menubar dreht sich bei mouseover

---
+++ Unity/Demo/Assets/Scripts/level.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class level : MonoBehaviour {
+    
+    /*
+    public void startgame()
+    {
+        Application.LoadLevel("demo");
+    }
+    */
+
+    void OnMouseUp()
+    {
+        Application.LoadLevel("demo");
+    } 
+}
