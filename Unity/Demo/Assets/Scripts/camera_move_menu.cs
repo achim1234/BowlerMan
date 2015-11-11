9f06e2ca6c228c5408fe18801161b98de38ddf94
Werner <Werner91@googlemail.com>
2015-11-11 22:17:14 +0100

    Menu Kamerabewegung

---
+++ Unity/Demo/Assets/Scripts/camera_move_menu.cs
@@ -0,0 +1,65 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using System;
+
+public class camera_move_menu : MonoBehaviour
+{
+
+    public float speed = 1.0f;
+    public float target_z;
+    public float target_minus_z;
+    Vector3 target = Vector3.zero;
+    Vector3 target_minus = Vector3.zero;
+    float step;
+
+    bool targetreached_right = true;
+    bool targetreached_left = false;
+
+
+
+
+    void Start()
+    {
+        target = this.transform.position + new Vector3(0, 0, target_z);
+        target_minus = this.transform.position + new Vector3(0, 0, target_minus_z);
+    }
+
+    void Update()
+    {
+        step = speed * Time.deltaTime;
+
+        if (targetreached_right)
+        {
+            Targetreached_left();
+        }
+        if (targetreached_left)
+        {
+            Targetreached_right();
+        }
+    }
+
+    void Targetreached_right()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target, step);
+        if (Mathf.Approximately(transform.position.z, target.z))
+        {
+            targetreached_right = true;
+            targetreached_left = false;
+        }
+    }
+
+    void Targetreached_left()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target_minus, step);
+        if (Mathf.Approximately(transform.position.z, target_minus.z))
+        {
+            targetreached_left = true;
+            targetreached_right = false;
+        }
+    }
+
+
+
+}
+
--- Unity/Demo/Assets/Scripts/mouse_over_manu.cs
+++ Unity/Demo/Assets/Scripts/mouse_over_manu.cs
@@ -1,60 +1,30 @@
-﻿using UnityEngine;
+﻿
+using UnityEngine;
 using System.Collections;
 
-public class mouse_over_manu : MonoBehaviour {
-    
-    public float z_value;
+public class mouse_over_manu : MonoBehaviour
+{
+
+    public float rotation_speed = 300;
+    public Quaternion z_value;
 
     void Start()
     {
-        z_value = this.transform.rotation.z;
+        z_value = this.transform.rotation;
     }
 
-    
-    void OnMouseOver() { 
-       
-            transform.RotateAround(this.transform.position, this.transform.forward, 100 * Time.deltaTime);
-          
+
+    void OnMouseOver()
+    {
+
+        transform.RotateAround(this.transform.position, this.transform.forward, rotation_speed * Time.deltaTime);
+
     }
 
     void OnMouseExit()
     {
-        transform.Rotate(0, 0, z_value);
+        transform.rotation = z_value;
     }
-    
-    /*
-	// Update is called once per frame
-	void Update () {
-        while (z_value < z_value + 360)
-        {
-            transform.RotateAround(this.transform.position, this.transform.forward, 100 * Time.deltaTime);
-        }
-   }
-    */
 
-  
-        public float rotationleft = 360;
-        public float rotationspeed = 10;
-        public float rotation;
-
-
-    /*
-    void Update() { 
-    
-
-        float rotation = rotationspeed * Time.deltaTime;
-        if (rotationleft > rotation)
-        {
-            rotationleft -= rotation;
-        }
-        else
-        {
-            rotation = rotationleft;
-            rotationleft = 0;
-        }
-        transform.Rotate(0, 0, rotation);
-    
-
-    }
-    */
 }
+
