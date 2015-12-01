2cea9b275b741b64e1187549ed6ffa58e0fbffed
Werner <Werner91@googlemail.com>
2015-12-01 22:09:53 +0100

    Soundmanager hinzugefügt

---
+++ Unity/Demo/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour {
+
+    public AudioSource efxSource;                           //Drag a reference to the audio source which will play the sound effects.
+    public AudioSource musicSource;                 //Drag a reference to the audio source which will play the music.
+
+
+    public AudioClip woohoo;                      //Bei klick auf Bowlerman
+
+
+    public static SoundManager instance = null;     //Allows other scripts to call functions from SoundManager.             
+ 
+
+
+    void Awake()
+    {
+        //Check if there is already an instance of SoundManager
+        if (instance == null)
+            //if not, set it to this.
+            instance = this;
+        //If instance already exists:
+        else if (instance != this)
+            //Destroy this, this enforces our singleton pattern so there can only be one instance of SoundManager.
+            Destroy(gameObject);
+
+        //Set SoundManager to DontDestroyOnLoad so that it won't be destroyed when reloading our scene.
+        //DontDestroyOnLoad(gameObject);
+    }
+
+
+    //Used to play single sound clips.
+    public void PlaySingle(string soundevent)
+    {
+
+        switch (soundevent)
+        {
+            case "woohoo_sound":
+                //Debug.Log("case tritt ein");
+                efxSource.clip = woohoo;
+                efxSource.Play();
+                break;
+
+            default:
+                Debug.Log("Default");
+                break;
+        }
+    }
+}
--- Unity/Demo/Assets/Scripts/woohoo.cs
+++ Unity/Demo/Assets/Scripts/woohoo.cs
@@ -3,17 +3,15 @@ using System.Collections;
 
 public class woohoo : MonoBehaviour {
 
-    public AudioClip myclip;
 
     // Use this for initialization
     void Start()
     {
-        this.gameObject.AddComponent<AudioSource>();
-        this.GetComponent<AudioSource>().clip = myclip;
+        
     }
 
     void OnMouseDown()
     {
-        this.GetComponent<AudioSource>().Play();
+        SoundManager.instance.PlaySingle("woohoo_sound");
     }
 }
