188891ad38863c826a2ee53f0c3a4515f409fd9e
Daniel Schwenk <mail@danielschwenk.de>
2015-12-15 11:06:21 +0100

    if game is over, player gets slowed down

--- Unity/Demo/Assets/Scripts/PlayerController.cs
+++ Unity/Demo/Assets/Scripts/PlayerController.cs
@@ -159,7 +159,12 @@ public class PlayerController : MonoBehaviour {
                     rb.AddForce(0, jumpForce, 0, ForceMode.Impulse);
                 }
             }
-        }      
+        }
+        else // game is over
+        {
+            // slightly slow down player
+            rb.velocity = rb.velocity * 0.985f;
+        } 
     }
 
     void OnCollisionEnter(Collision collision)
