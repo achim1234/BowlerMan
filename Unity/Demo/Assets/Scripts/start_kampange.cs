cd9b5d004d99c1e5fb28c377400c0c908ae26123
Werner <Werner91@googlemail.com>
2016-01-03 18:09:58 +0100

    - menu auf Kampagne und Single Game umgebaut

---
+++ Unity/Demo/Assets/Scripts/start_kampange.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class start_kampange : MonoBehaviour {
+
+    void OnMouseUp()
+    {
+        SoundManager.instance.PlaySingle("stop_music");
+        Application.LoadLevel("achims_level_2");
+    }
+}
